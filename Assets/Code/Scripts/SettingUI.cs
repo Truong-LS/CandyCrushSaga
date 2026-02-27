f60a9383a97bbe83bcefb14fe59aabcb41932367
tuankiet-hello <tuankiet.08102004@gmail.com>
2026-02-28 04:28:49 +0700

    SettingUI

---
+++ Assets/Code/Scripts/SettingUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        // Lấy dữ liệu đã lưu
+        float music = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        float sfx = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        musicSlider.value = music;
+        sfxSlider.value = sfx;
+
+        musicSlider.onValueChanged.AddListener(OnMusicChanged);
+        sfxSlider.onValueChanged.AddListener(OnSFXChanged);
+    }
+
+    void OnMusicChanged(float value)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", value);
+        Debug.Log("Music: " + value);
+    }
+
+    void OnSFXChanged(float value)
+    {
+        PlayerPrefs.SetFloat("SFXVolume", value);
+        Debug.Log("SFX: " + value);
+    }
+
+    public void CloseSetting()
+    {
+        gameObject.SetActive(false);
+    }
+}
